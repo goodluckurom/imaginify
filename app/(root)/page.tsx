03d13b426e8025863317e1897b4cc049b145c519
goodluckurom <urosoft.me@gmail.com>
2024-06-01 06:09:54 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <p>welcome to the home page of our application</p>
+    </div>
+  );
+};
+
+export default page;
